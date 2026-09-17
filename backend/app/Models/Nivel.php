178a0efa89c1e5a089d4ab7098c71e6de6a67375
SlarionM <pierocoa9@gmail.com>
2026-09-17 15:05:54 -0500

    feat: backend nivel

---
+++ backend/app/Models/Nivel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Nivel extends Model
+{
+    /** @use HasFactory<\Database\Factories\NivelFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+    ];
+}
---
+++ backend/database/factories/NivelFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Nivel;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Nivel>
+ */
+class NivelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
